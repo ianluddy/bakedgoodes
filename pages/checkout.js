f0d0f40b5e6e9b1fee7758784745a59acb08d645
Ian Luddy <iluddy@zendesk.com>
2022-03-06 22:21:53 +0000

    New pages

---
+++ pages/checkout.js
@@ -0,0 +1,12 @@
+import Layout from '../components/layout';
+import FadeIn from 'react-fade-in';
+
+export default function() {
+  return (
+    <Layout>
+      <FadeIn delay="0">
+        <h1>Checkout</h1>
+      </FadeIn>
+    </Layout>
+  );
+}
---
+++ pages/contact.js
@@ -0,0 +1,12 @@
+import Layout from '../components/layout';
+import FadeIn from 'react-fade-in';
+
+export default function() {
+  return (
+    <Layout>
+      <FadeIn delay="0">
+        <h1>Contact</h1>
+      </FadeIn>
+    </Layout>
+  );
+}
