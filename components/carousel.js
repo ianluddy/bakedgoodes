2a8c9674f9b14202f8cec820f3b16bedcc0b15ac
Ian Luddy <iluddy@zendesk.com>
2022-04-06 09:56:45 +0100

    Carousel changes

--- components/carousel.js
+++ components/carousel.js
@@ -1,61 +1,72 @@
 import styled from 'styled-components';
 import theme from '../themes/default';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Autoplay, Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
 
 import Image from './image';
 import ProductTile from './productTile';
 
-export default ({ products, excludeId, productType }) => {
+const Wrapper = styled.div`
+  text-align: center;
+`;
+
+export default ({ title, products, excludeId }) => {
   return (
-    <Swiper
-      modules={[Pagination]}
-      spaceBetween={10}
-      slidesPerView={2}
-      loop={true}
-      pagination={{ clickable: true }}
-      breakpoints={{
-        [theme.breakpoints.xs]: {
-          slidesPerView: 2,
-          spaceBetween: 10
-        },
-        [theme.breakpoints.sm]: {
-          slidesPerView: 2,
-          spaceBetween: 10
-        },
-        [theme.breakpoints.md]: {
-          slidesPerView: 3,
-          spaceBetween: 10
-        },
-        [theme.breakpoints.lg]: {
-          slidesPerView: 4,
-          spaceBetween: 10
-        },
-        [theme.breakpoints.xl]: {
-          slidesPerView: 4,
-          spaceBetween: 10
-        }
-      }}
-    >
-    {
-      products.filter(product =>
-        product.id != excludeId
-      ).map(product => (
-        <SwiperSlide key={product.id}>
-          <ProductTile 
-            key={product.id}
-            path={`/${productType}/${product.id}`}
-            title={product.meta.title}
-            caption={product.meta.caption}
-            desc={product.meta.desc}
-            src={product.meta.src}
-            link={product.meta.link}
-          />
-        </SwiperSlide>
-      ))
-    }
-    </Swiper>
+    <Wrapper>
+      <h2>
+        {title}
+      </h2>
+      <Swiper
+        modules={[Pagination, Autoplay]}
+        spaceBetween={10}
+        slidesPerView={2}
+        loop={true}
+        autoplay={true}
+        pagination={{ clickable: true }}
+        breakpoints={{
+          [theme.breakpoints.xs]: {
+            slidesPerView: 2,
+            spaceBetween: 10
+          },
+          [theme.breakpoints.sm]: {
+            slidesPerView: 2,
+            spaceBetween: 10
+          },
+          [theme.breakpoints.md]: {
+            slidesPerView: 3,
+            spaceBetween: 10
+          },
+          [theme.breakpoints.lg]: {
+            slidesPerView: 4,
+            spaceBetween: 10
+          },
+          [theme.breakpoints.xl]: {
+            slidesPerView: 4,
+            spaceBetween: 10
+          }
+        }}
+      >
+      {
+        products.filter(product =>
+          product.id != excludeId
+        ).map(product => (
+          <SwiperSlide key={product.id}>
+            <ProductTile 
+              key={product.id}
+              path={product.meta.path}
+              title={product.meta.title}
+              caption={product.meta.caption}
+              desc={product.meta.desc}
+              src={product.meta.src}
+              link={product.meta.link}
+            />
+          </SwiperSlide>
+        ))
+      }
+      </Swiper>
+    </Wrapper>
   );
 };
